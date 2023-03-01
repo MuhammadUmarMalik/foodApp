dc3ba360ab0ea6a1c163c9896ad3043d84a0b403
Muhammad Umar Malik <89419975+MuhammadUmarMalik@users.noreply.github.com>
2023-03-01 18:17:55 +0500

    input

--- src/components/login.js
+++ src/components/login.js
@@ -1,10 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import React from "react";
 
 const login = () => {
   return (
     <View>
       <Text>login</Text>
+      <Text>Umar </Text>
+      <TextInput placeholder=""></TextInput>
     </View>
   );
 };
